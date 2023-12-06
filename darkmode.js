18da7684be41a2b9ee990c4cc6372706ee22998a
Zach1031 <zachdef31@gmail.com>
2023-12-05 20:26:35 -0500

    Cleaned up js file

--- darkmode.js
+++ darkmode.js
@@ -1,15 +1,18 @@
 var logoSrc = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Purdue_Boilermakers_primary_mark_color_on-black.svg/2560px-Purdue_Boilermakers_primary_mark_color_on-black.svg.png";
-  
 
-var test = document.querySelector('d2l-navigation-main-header');
-  var test2 = test.querySelector('.d2l-navigation-header-left');
-  var test3 = test2.querySelector('.d2l-navigation-s-header-logo-area');
-  var test4 = test3.querySelector('d2l-navigation-link-image');
+
+var logoParent = document.querySelector('.d2l-navigation-s-header-logo-area');
+var oldLogo = logoParent.querySelector('d2l-navigation-link-image');
+
+if (!logoParent.querySelector('img')) {
   let logo = new Image();
   logo.src = logoSrc;
   logo.classList.add('logo');
-  if (test4 != null)
-    test4.remove();
-  //test3.appendChild(logo);
-  test3.insertBefore(logo, test3.querySelector('d2l-navigation-separator').nextSibling);
+  logoParent.insertBefore(logo, logoParent.querySelector('d2l-navigation-separator').nextSibling);
+
+  if (oldLogo != null) {
+    oldLogo.remove();
+  }
+
+}
 
